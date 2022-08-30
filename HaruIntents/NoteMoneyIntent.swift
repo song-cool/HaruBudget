dc06686e13cf9448f53b4919aaf1018cf53fa6e9
song-cool <oscar2978@naver.com>
2022-08-30 16:44:03 +0900

    [FEAT] intent 시도..

---
+++ HaruIntents/NoteMoneyIntent.swift
@@ -0,0 +1,13 @@
+//
+//  NoteMoneyIntent.swift
+//  HaruBudgetWatch
+//
+//  Created by 송시원 on 2022/08/29.
+//
+
+import Intents
+import SwiftUI
+
+//struct StartMeditationIntent: Intents {
+//    static let title: LocalizedStringResource = "Start Meditation Session"
+//}
